c30c584d2525577bcd5cfae6235433ef673e8a47
Jarvis FW <86500890+JarvisFW@users.noreply.github.com>
2022-01-17 22:12:38 +0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'Jarvis'
+description 'qb-underwaterweed'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua'
+}
