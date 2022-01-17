37cb293efa9d7304f6a44e14e1dceab276e01e96
Jarvis FW <86500890+JarvisFW@users.noreply.github.com>
2022-01-18 03:12:01 +0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = Config or {}
+
+
+Config.Delays = {
+	WeedProcessing = 1000 * 3
+}
+
+
+Config.CircleZones3 = {
+
+	WeedField = {coords = vector3(2927.13, -857.05, -14.79), name = 'blip_weedfield', color = 25, sprite = 496, radius = 50.0},
+	
+	WeedProcessing = {coords = vector3(1033.84, -3207.43, -38.18), name = 'blip_weedprocessing', color = 0, sprite = 6, radius = 10.0},
+	
+	DrugDealer = {coords = vector3(-1172.02, -1571.98, 4.66), name = 'blip_drugdealer', color = 6, sprite = 378, radius = 25.0},
+}
